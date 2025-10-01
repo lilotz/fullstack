f77a624cc17833a7cabd20fcbebc1944b5632d35
lisalotz <lisa.lotz@tu-dortmund.de>
2025-10-01 18:20:37 +0300

    exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -49,6 +49,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
 /*app.post('/api/persons', (request, response) => {
     const body = request.body
 
