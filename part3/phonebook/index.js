f028e008be9c981b4a93319bd66832087aec91d8
lisalotz <lisa.lotz@tu-dortmund.de>
2025-10-01 18:17:08 +0300

    exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -37,6 +37,18 @@ app.get('/info', (request, response) => {
         `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
 /*app.post('/api/persons', (request, response) => {
     const body = request.body
 
