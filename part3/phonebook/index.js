117b3b95a53a30f14976ae4b48aa32246d4e04b8
lisalotz <lisa.lotz@tu-dortmund.de>
2025-10-01 19:42:27 +0300

    exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,11 @@ const app = express()
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', function getBody (request) {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
