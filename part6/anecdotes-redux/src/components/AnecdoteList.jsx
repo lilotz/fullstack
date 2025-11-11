8dfc3ad702165381282d2fcf8c2476a3a9e84966
lisalotz <lisa.lotz@tu-dortmund.de>
2025-11-11 15:48:40 +0200

    exercise 6.9

--- part6/anecdotes-redux/src/components/AnecdoteList.jsx
+++ part6/anecdotes-redux/src/components/AnecdoteList.jsx
@@ -4,15 +4,21 @@ import { useSelector, useDispatch } from 'react-redux'
 const AnecdoteList = () => {
   const dispatch = useDispatch()
 
-  const anecdotes = useSelector(state => state)
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  const anecdotes = useSelector(state => {
+    if (state.filter === '') {
+      return [...state.anecdotes].sort((a, b) => b.votes - a.votes)
+    }
+    else {
+      return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+    }
+  })
 
   const vote = (id) => { dispatch(changeVote(id)) }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote => (
+      {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
